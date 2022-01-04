610c58fc0361261e42de7c45363c888a37fc4451
Van Vo <frontendvan@outlook.com>
2022-01-04 15:13:12 +0000

    master

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=animals')
   .then(response => response.json())
-  .then(data => document.body.style.backgroundImage = `url(${data.urls.full})`);
+  .then(data => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    let author = document.getElementById("author")
+    author.textContent = `By ${data.user.name}`
+    
+    
+  })
