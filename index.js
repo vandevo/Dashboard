20fb94c66fb08a7cdf64f32d6d4e03fc9fa4f2a6
Van Vo <frontendvan@outlook.com>
2022-02-18 15:17:13 +0000

    master

--- index.js
+++ index.js
@@ -47,20 +47,24 @@ document.getElementById('time').textContent = date.toLocaleTimeString("en-us", {
 
 setInterval(getCurrentTime, 1000) 
 
-
-
 navigator.geolocation.getCurrentPosition(position => {
   fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-    .then(res =>  {
-      if (!res.okay) {
-        throw Error("Weather data is not available")
-      }
-      return res.jason()
-    })
-    .then(data => {
-      console.log(data)
-    })
-
-
+      .then(res => {
+          if (!res.ok) {
+              throw Error("Weather data not available")
+          }
+          return res.json()
+      })
+      .then(data => {
+          console.log(data)
+          const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+          document.getElementById("weather").innerHTML = `
+              <img src=${iconUrl} />
+              <p>${Math.round(data.main.temp)}º</p>
+              <p>${data.name}</p>
+          `
+      })
+      .catch(err => console.error(err))
 });
+
 
